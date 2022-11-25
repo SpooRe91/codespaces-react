f82cb10af5b6728e904fb67b936748fc2e1518a4
Martin B <85784810+SpooRe91@users.noreply.github.com>
2022-11-25 15:08:31 +0000

    created a simple countdown timer!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
+import { useEffect, useState, useRef } from 'react';
+
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const currRef = useRef(0);
+
+
+  const handleChange = (e) => {
+    currRef.current = e.target.value;
+    setCount(state => e.target.value);
+  }
+
+  const handleStart = (e) => {
+    setIsActive(state => true);
+    currRef.current = 0;
+  }
+
+
+  useEffect(() => {
+    if (count === 0) { return setIsActive(state => false) }
+    
+    if (isActive) {
+      const interval = setInterval(() => {
+        setCount(prev => prev - 1);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+
+      };
+    }
+  }, [isActive, count])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <input ref={currRef} onChange={handleChange} type={"number"} value={currRef.current} />
+      {/* <button onClick={handleStart}>Set</button> */}
+      <button onClick={handleStart}>Start</button>
+      {
+        isActive
+          ?
+          <p style={{ "background": "black", "color": "white" }}>{count}</p>
+          :
+          <p style={{ "background": "black", "color": "white" }}>Timer over!</p>
+      }
     </div>
   );
 }
